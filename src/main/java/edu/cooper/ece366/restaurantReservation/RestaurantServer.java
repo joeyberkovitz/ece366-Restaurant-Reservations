7dd6c8b770b001fe5d85b947d03d48508fb14877
cooper954 <60748041+cooper954@users.noreply.github.com>
2020-02-09 20:56:41 -0500

    Add build file and server stub

---
+++ src/main/java/edu/cooper/ece366/restaurantReservation/RestaurantServer.java
@@ -0,0 +1,74 @@
+package edu.cooper.ece366.restaurantReservation;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+/**
+ * Server that manages startup/shutdown of a {@code Greeter} server.
+ */
+public class RestaurantServer {
+  private static final Logger logger = Logger.getLogger(RestaurantServer.class.getName());
+
+  private Server server;
+
+  private void start() throws IOException {
+    /* The port on which the server should run */
+    int port = 50051;
+    server = ServerBuilder.forPort(port)
+        .addService(new RestaurantServiceImpl())
+        .build()
+        .start();
+    logger.info("Server started, listening on " + port);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          RestaurantServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
+      }
+    });
+  }
+
+  private void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
+
+  /**
+   * Await termination on the main thread since the grpc library uses daemon threads.
+   */
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  /**
+   * Main launches the server from the command line.
+   */
+  public static void main(String[] args) throws IOException, InterruptedException {
+    final RestaurantServer server = new RestaurantServer();
+    server.start();
+    server.blockUntilShutdown();
+  }
+
+  static class RestaurantServiceImpl extends RestaurantServiceGrpc.RestaurantServiceImplBase {
+
+    @Override
+    public void createRestaurant(RestaurantServiceOuterClass.Restaurant req, StreamObserver<RestaurantServiceOuterClass.Restaurant> responseObserver) {
+      RestaurantServiceOuterClass.Restaurant reply = RestaurantServiceOuterClass.Restaurant.newBuilder().setId(1).build();
+      responseObserver.onNext(reply);
+      responseObserver.onCompleted();
+    }
+  }
+}
