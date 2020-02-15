3f0e52cfb6b669356a102fe0399602a19c27630e
cooper954 <60748041+cooper954@users.noreply.github.com>
2020-02-14 21:11:48 -0500

    Accept all (multiple) parameters for restaurant DAO insert

--- grpc/src/main/java/edu/cooper/ece366/restaurantReservation/grpc/Restaurants/RestaurantDao.java
+++ grpc/src/main/java/edu/cooper/ece366/restaurantReservation/grpc/Restaurants/RestaurantDao.java
@@ -2,11 +2,12 @@ package edu.cooper.ece366.restaurantReservation.grpc.Restaurants;
 
 import edu.cooper.ece366.restaurantReservation.grpc.Restaurant;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface RestaurantDao {
-	@SqlUpdate("INSERT INTO restaurant(name) VALUES (:name)")
+	@SqlUpdate("INSERT INTO restaurant(name, address, contact, category) VALUES (:name, :address_id, :contact_id :category)")
 	@GetGeneratedKeys("id")
-	int insertBean(@BindBean Restaurant restaurant);
+	int insertRestaurant(@Bind("address_id") int address_id, @Bind("contact_id") int contact_id, @BindBean Restaurant restaurant);
 }
