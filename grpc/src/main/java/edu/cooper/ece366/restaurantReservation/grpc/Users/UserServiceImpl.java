8feb5aff32e16579238d044cb36801bb3a34b3d4
Sam Maltz <samuelmaltz@gmail.com>
2020-02-17 22:25:48 -0500

    started setUser

--- grpc/src/main/java/edu/cooper/ece366/restaurantReservation/grpc/Users/UserServiceImpl.java
+++ grpc/src/main/java/edu/cooper/ece366/restaurantReservation/grpc/Users/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package edu.cooper.ece366.restaurantReservation.grpc.Users;
 
+import edu.cooper.ece366.restaurantReservation.grpc.User;
 import edu.cooper.ece366.restaurantReservation.grpc.UserServiceGrpc;
 import org.jdbi.v3.core.Jdbi;
 
@@ -9,5 +10,16 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
 
     public UserServiceImpl(Jdbi db){
         this.db = db;
+    }
+
+    @Override
+    public User SetUser(User user) {
+        UserManager um = new UserManager(db);
+        if (user.getId() == 0) {
+            //todo throw error
+        }
+        else {
+
+        }
     }
 }
