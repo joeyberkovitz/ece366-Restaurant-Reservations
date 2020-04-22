f179ceefe4219cf9bb8297e27be27081095f4a6b
Joey Berkovitz <joeyberkovitz@gmail.com>
2020-04-22 17:46:02 -0400

    Fix home

--- spring/src/main/js/reservdjs/src/views/Home.vue
+++ spring/src/main/js/reservdjs/src/views/Home.vue
@@ -2,8 +2,8 @@
   <div class="home">
     <h1>Welcome to RESERVD</h1>
     <h2>Login or Register to get started!</h2>
-    <ul>
-      <lh>With RESERVD you can:</lh>
+    <h3>With RESERVD you can:</h3>
+    <ul style="list-style-type:none;">
       <li>Make reservations at various restaurants</li>
       <li>Invite others to your reservations</li>
       <li>Manage your restaurant's reservations</li>
